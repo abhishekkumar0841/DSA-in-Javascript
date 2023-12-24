1cd748eb0ed4d2244d119c0aede780e473d03e9c
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-24 18:05:30 +0530

    learn how to count chars with only alphanumeric using charCodeAt() built in function or method

--- script.js
+++ script.js
@@ -23,3 +23,34 @@ console.log(addTillN2(1000000000));
 
 let t4 = performance.now();
 console.log(`Timer stops: ${(t4 - t3) / 1000}`);
+
+//***************************************************************** */
+
+//Count the characters occurrences only alphanumeric a-z A-Z or 0-9
+function countChars(str) {
+  let allStr = str.toLowerCase();
+  let lookup = {};
+  for (let i = 0; i < allStr.length; i++) {
+    if(isAlphaNumeric(allStr[i])){
+      lookup[allStr[i]] ? lookup[allStr[i]]++ : (lookup[allStr[i]] = 1);
+    }
+  }
+  return lookup;
+}
+
+//this function only return the alphanumeric characters
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && //numeric (0 - 9)
+    !(code > 64 && code < 91) && //upper alpha (A - Z)
+    !(code > 96 && code < 123) //lower alpha (a - z)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+console.log(
+  countChars("Count the characters occurrences @@only alphanumeric!!! is this correct or ### ---- not 😒😒")
+);
